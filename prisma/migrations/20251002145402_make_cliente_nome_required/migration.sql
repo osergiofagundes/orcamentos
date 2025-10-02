28db8af2f07c65d816a44da178323b4090153d6d
Sérgio Fagundes <contato.sergiofagundes@gmail.com>
2025-10-02 17:52:10 -0300

    Fix deploy

--- prisma/migrations/20251002145402_make_cliente_nome_required/migration.sql
+++ prisma/migrations/20251002145402_make_cliente_nome_required/migration.sql
@@ -4,5 +4,11 @@
   - Made the column `cliente_nome` on table `orcamento` required. This step will fail if there are existing NULL values in that column.
 
 */
+
+-- First, update any existing NULL values to a default value
+UPDATE "public"."orcamento" 
+SET "cliente_nome" = 'Cliente não informado'
+WHERE "cliente_nome" IS NULL;
+
 -- AlterTable
 ALTER TABLE "public"."orcamento" ALTER COLUMN "cliente_nome" SET NOT NULL;
